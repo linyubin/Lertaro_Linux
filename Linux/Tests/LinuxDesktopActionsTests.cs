7075d8cbf56752f97c573db249736b21cae0455a
_linyb <linyb.1420@outlook.com>
2026-09-18 02:25:45 +0800

    test: cover Linux desktop application launch

--- Linux/Tests/LinuxDesktopActionsTests.cs
+++ Linux/Tests/LinuxDesktopActionsTests.cs
@@ -19,6 +19,26 @@ public sealed class LinuxDesktopActionsTests
     }
 
     [TestMethod]
+    public void BuildLaunchApplicationStartInfo_UsesGioWithoutShellParsing()
+    {
+        var desktopFile = Path.GetFullPath("applications/My App.desktop");
+
+        var info = LinuxDesktopActions.BuildLaunchApplicationStartInfo(desktopFile);
+
+        Assert.AreEqual("gio", info.FileName);
+        Assert.IsFalse(info.UseShellExecute);
+        Assert.HasCount(2, info.ArgumentList);
+        Assert.AreEqual("launch", info.ArgumentList[0]);
+        Assert.AreEqual(desktopFile, info.ArgumentList[1]);
+    }
+
+    [TestMethod]
+    public void BuildLaunchApplicationStartInfo_RejectsNonDesktopFile()
+    {
+        Assert.Throws<ArgumentException>(() => LinuxDesktopActions.BuildLaunchApplicationStartInfo("not-an-app.txt"));
+    }
+
+    [TestMethod]
     public void BuildRevealStartInfo_UsesFreedesktopFileManagerDbusInterface()
     {
         var path = Path.GetFullPath("folder/report.txt");
